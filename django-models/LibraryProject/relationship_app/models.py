b40ecc35b2749ddde70f1bfd7ab4490e915924e0
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-14 23:55:47 +0200

    add: Advanced Model Relationships

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,6 +14,6 @@ class Library(models.Model):
     name= models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name="libraries")
 
-class Librarian (models.Model):
+class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
