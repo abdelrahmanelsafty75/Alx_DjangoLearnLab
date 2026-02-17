8954d1c4f8756f11a4e50a17cfd1aa62d3426626
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-18 01:28:20 +0200

    views

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import get_all_books, LibraryDetailView, list_books
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]       
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author, Book ,Library, Librarian
+from django.views.generic import DetailView
+def list_books(request):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'books.html', context)
 
-# Create your views here.
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
