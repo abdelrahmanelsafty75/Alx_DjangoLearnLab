845687079097dcd138d18f09c1bffdbb96f2de1a
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-18 02:57:16 +0200

    User Authentication(Register,login and logout)

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+                        # views.register(for function) = RegisterView.as_view(for class)
 ]       
 
