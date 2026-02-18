b0341fcb77044480762f55e483e3026546b27918
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-18 02:51:57 +0200

    User Authentication(Register,login and logout)

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 def list_books(request):
     books = Book.objects.all()
     context = {
@@ -13,3 +18,8 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+class RegisterView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
