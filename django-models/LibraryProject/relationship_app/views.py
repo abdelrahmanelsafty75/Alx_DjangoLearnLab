f05fb6e7b8be12ac2b1e3be69b5d57202f1d7d6d
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-18 02:54:30 +0200

    User Authentication(Register,login and logout)

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
