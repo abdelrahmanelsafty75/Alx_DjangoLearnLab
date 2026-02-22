a8404fae902639565b5253ea0b7fd0b706570ca8
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-22 04:34:06 +0200

    integraion into admin

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import Author, Book, Library, Librarian, UserProfile
+from .models import Author, Book, Library, Librarian, UserProfile, CustomUser
+from django.contrib.auth.admin import UserAdmin
+
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Library)
 admin.site.register(Librarian)
-
 admin.site.register(UserProfile)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -47,13 +48,13 @@ ROLE_CHOICES = [
 ]
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
