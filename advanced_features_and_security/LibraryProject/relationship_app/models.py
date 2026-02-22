fcc6d63c9b6f94dc6d1246f507e199a0c753f225
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-22 04:47:45 +0200

    integraion into admin

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.db import models
 
 
@@ -48,13 +47,13 @@ ROLE_CHOICES = [
 ]
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
