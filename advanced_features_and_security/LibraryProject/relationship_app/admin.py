8da13fc1172fc7986f1c5d6824f12afae942ff34
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-02-22 04:50:25 +0200

    integraion into admin

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Author, Book, Library, Librarian, UserProfile, CustomUser
-from django.contrib.auth.admin import UserAdmin
+from .models import Author, Book, Library, Librarian, UserProfile
 
 
 admin.site.register(Author)
