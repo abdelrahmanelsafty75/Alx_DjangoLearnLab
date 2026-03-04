804f2f79176a9bc3219216ab2c57d9cb8dcd4c0e
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-03-04 15:44:46 +0200

    authentications and permissions

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,6 +4,8 @@ from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -11,6 +13,8 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  
     # Include the router URLs for BookViewSet (all CRUD operations)
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+
     
     path('', include(router.urls)),  # This includes all routes registered with the router
 ]
