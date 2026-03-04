79784d67ffa262beb55628f52e46d4567b00bab3
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-03-05 00:28:36 +0200

    test

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Author, Book
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age')
+    search_fields = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):  
+    list_display = ('id', 'title', 'author', 'publication_year')
+    search_fields = ('title',)
+    list_filter = ('publication_year',)
